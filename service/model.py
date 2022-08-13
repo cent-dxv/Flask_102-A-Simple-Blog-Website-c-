89d858170753ad7a27d80817239f7333628fef00
cent-dxv <centdxv@gmail.com>
2022-08-13 14:19:22 -0300

    commnet table in db

--- service/model.py
+++ service/model.py
@@ -11,9 +11,22 @@ class User(db.Model , UserMixin):
     password = db.Column(db.String(120), unique=True)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     post =db.relationship('Post' , backref='user' , passive_deletes =True)
+    Comments =db.relationship('Comment' , backref='user' , passive_deletes =True)
+
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     post_text =db.Column(db.String(255), nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     author = db.Column(db.Integer , db.ForeignKey('user.id' ,ondelete="CASCADE") ,nullable=False)
+    Comments =db.relationship('Comment' , backref='post' , passive_deletes =True)
+
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id',ondelete="CASCADE"), nullable=False)
+    post_id =db.Column(db.Integer, db.ForeignKey('post.id',ondelete="CASCADE"), nullable=False)
+    text = db.Column(db.String(255), nullable=False)
